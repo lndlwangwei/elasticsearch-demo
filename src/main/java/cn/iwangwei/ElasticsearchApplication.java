5262ef96620414857ceb3a72de591572869f7d02
lndlwangwei <lndl.wangwei@gmail.com>
2018-07-31 23:21:21 +0800

    添加elasticsearch基础配置

--- src/main/java/cn/iwangwei/ElasticsearchApplication.java
+++ src/main/java/cn/iwangwei/ElasticsearchApplication.java
@@ -1,10 +1,11 @@
 package cn.iwangwei;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
+@EnableElasticsearchRepositories(basePackages = "cn.iwangwei.es.repository")
 public class ElasticsearchApplication {
 
     public static void main(String[] args) {
--- src/main/java/cn/iwangwei/controller/TestController.java
+++ src/main/java/cn/iwangwei/controller/TestController.java
@@ -4,7 +4,6 @@ import cn.iwangwei.es.bean.Resource;
 import cn.iwangwei.es.service.ResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +17,7 @@ public class TestController {
         return "hello world";
     }
 
-    @PutMapping("/resources")
+    @GetMapping("/resources")
     public Resource add() {
         Resource resource = new Resource();
         resource.setId(1);
